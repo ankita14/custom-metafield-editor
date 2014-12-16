57413c34bd2360abc6311e786ae77d95048173a7
Ankita14 <ankiagrawal19@gmail.com>
2014-12-16 15:42:00 +0530

    code for embedded app added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  layout :false
+
   def new
     authenticate if params[:shop].present?
   end
@@ -27,26 +29,47 @@ class SessionsController < ApplicationController
   end
   
   protected
-  
+
   def authenticate
+    #
+    # Instead of doing a backend redirect we need to do a javascript redirect
+    # here. Open the app/views/commom/iframe_redirect.html.erb file to understand why.
+    #
     if shop_name = sanitize_shop_param(params)
-      redirect_to "/auth/shopify?shop=#{shop_name}"
+      @redirect_url = "/auth/shopify?shop=#{shop_name}"
+      render "/common/iframe_redirect"
     else
       redirect_to return_address
     end
   end
   
+  # def authenticate
+  #   if shop_name = sanitize_shop_param(params)
+  #     redirect_to "/auth/shopify?shop=#{shop_name}"
+  #   else
+  #     redirect_to return_address
+  #   end
+  # end
+  
   def return_address
     session[:return_to] || root_url
   end
-  
+
   def sanitize_shop_param(params)
     return unless params[:shop].present?
     name = params[:shop].to_s.strip
     name += '.myshopify.com' if !name.include?("myshopify.com") && !name.include?(".")
-    name.sub!(%r|https?://|, '')
-
-    u = URI("http://#{name}")
-    u.host.ends_with?(".myshopify.com") ? u.host : nil
+    name.sub('https://', '').sub('http://', '')
   end
 end
+  
+#   def sanitize_shop_param(params)
+#     return unless params[:shop].present?
+#     name = params[:shop].to_s.strip
+#     name += '.myshopify.com' if !name.include?("myshopify.com") && !name.include?(".")
+#     name.sub!(%r|https?://|, '')
+
+#     u = URI("http://#{name}")
+#     u.host.ends_with?(".myshopify.com") ? u.host : nil
+#   end
+# end
